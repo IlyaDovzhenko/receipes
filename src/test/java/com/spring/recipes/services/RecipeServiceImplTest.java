687c415e2670f1196011b0fb963084f19c2e9cd0
ilya <i.dovzhenko26@gmail.com>
2019-12-19 11:33:27 +0700

    Added tests for recipe service

---
+++ src/test/java/com/spring/recipes/services/RecipeServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.spring.recipes.services;
+
+import com.spring.recipes.domain.Recipe;
+import com.spring.recipes.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    private RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+        //recipeService.getRecipes().forEach(System.out::println);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
