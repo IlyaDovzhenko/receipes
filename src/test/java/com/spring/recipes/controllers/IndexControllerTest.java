210325a28f14628f2cec9c16e0e5a141cfe4c35c
ilya <i.dovzhenko26@gmail.com>
2019-12-21 20:07:20 +0700

    Added IndexController tests

--- src/test/java/com/spring/recipes/controllers/IndexControllerTest.java
+++ src/test/java/com/spring/recipes/controllers/IndexControllerTest.java
@@ -1,16 +1,21 @@
 package com.spring.recipes.controllers;
 
+import com.spring.recipes.domain.Recipe;
 import com.spring.recipes.repositories.CategoryRepository;
 import com.spring.recipes.repositories.UnitOfMeasureRepository;
 import com.spring.recipes.services.RecipeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -41,7 +46,28 @@ class IndexControllerTest {
 
     @Test
     void getRecipes() {
-        assertEquals("recipes_list", indexController.getRecipes(model));
-        Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        recipes.add(recipe1);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        recipes.add(recipe2);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String viewName = indexController.getRecipes(model);
+
+        //then
+        assertEquals("recipes_list", viewName);
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        verify(recipeService, times(1)).getRecipes();
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+
     }
 }
